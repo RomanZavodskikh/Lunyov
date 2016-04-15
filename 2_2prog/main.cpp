63b644737b9ddf9bbebb3fbd2d1c16a9f5b8443a
RomanZavodskikh <roman.zavodskikh@phystech.edu>
2016-04-15 23:34:22 +0300

    Refactored the code.

--- 2_2prog/main.cpp
+++ 2_2prog/main.cpp
@@ -60,16 +60,22 @@ void* get_triangle(void* data)
     unsigned long const num_of_cpus = data_p->num_of_cpus; 
     unsigned long const cur_cpu = data_p->cur_cpu;
 
+    //we swap string if cur diagonal elem is 0
     pthread_mutex_lock(data_p->swap_mutex_p);
     if ((*m_p)(str_num, str_num) == 0)
     {
         swap_with_nonzero(data_p);
     }
     pthread_mutex_unlock(data_p->swap_mutex_p);
+
+    //go away if all elems lower is 0 - determinant is known. It's 0.
     if ((*m_p)(str_num, str_num) == 0)
     {
         return nullptr;
     }
+
+    //executing the one step of Gauss algo
+    //str_num is the lowest string that is computed correctly already
     for (unsigned long i = str_num+1+cur_cpu; i < m_p->size1(); i+=num_of_cpus)
     {
         T mul = (*m_p)(i, str_num) / (*m_p)(str_num, str_num);
@@ -84,23 +90,27 @@ void* get_triangle(void* data)
 
 int main(int argc, char** argv)
 {
+    //handling wrond num of args
     if (argc != 2)
     {
         cerr << "Usage: ./a.out NUM_OF_CPUS" << endl;
         exit(EXIT_FAILURE);
     }
 
+    //creating the vector of threads
     unsigned long num_of_cpus = strtoul_err(argv[1], nullptr, 10);
     num_of_cpus = min(num_of_cpus, 
         (unsigned long)std::thread::hardware_concurrency());
     std::vector<pthread_t> threads_v(num_of_cpus);
 
+    //initializing the matrix
     std::ifstream matrix_file;
     matrix_file.open("matrix2.txt");
     unsigned long m_size = 0;
     matrix_file >> m_size;
     matrix<T> matrix(m_size, m_size);
 
+    //reading the matrix
     for (unsigned long i = 0; i < m_size; ++i)
     {
         for (unsigned long j = 0; j < m_size; ++j)
@@ -109,16 +119,20 @@ int main(int argc, char** argv)
         }
     }
 
-    Data* data = new Data[num_of_cpus];
+    //initializing the swap mutex (need for Gauss algorithm)
     pthread_mutex_t swap_mutex;
     pthread_mutex_init(&swap_mutex, nullptr);
 
+    //calculating the args for threads
+    Data* data = new Data[num_of_cpus];
     for (unsigned long i = 0; i < num_of_cpus; ++i)
     {
         data[i].matrix_p = &matrix;
         data[i].num_of_cpus = num_of_cpus;
         data[i].swap_mutex_p = &swap_mutex;
     }
+
+    //executing the Gauss algo for creating the triangle matrix
     for (unsigned long i = 0; i < m_size; ++i)
     {
         for (unsigned long j = 0; j < num_of_cpus; ++j)
@@ -133,6 +147,7 @@ int main(int argc, char** argv)
         }
     }
 
+    //multiplying the diag elems in order to get determinant
     double det = 1;
     for (unsigned long i = 0; i < m_size; ++i)
     {
